f66842fc51c23625d9046d2a9551edbf42fdc8b5
biscuits-na <ttphangula@gmail.com>
2026-04-22 16:53:14 +0200

    Delete RentalManagementSystem directory

--- RentalManagementSystem/src/rentalmanagementsystem/Main.java
+++
@@ -1,9 +0,0 @@
-package rentalmanagementsystem;
-
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame());
-    }
-}
